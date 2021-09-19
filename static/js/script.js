3bc12a12214f6fd343eaf89454bac2144b1846bb
mahianmahin <80479590+mahianmahin@users.noreply.github.com>
2021-09-20 00:05:32 +0600

    dashboard done

--- static/js/script.js
+++ static/js/script.js
@@ -65,53 +65,68 @@ window.addEventListener('scroll', () => {
 // Star rating system
 
 // Star clicking effect
+try {
+    document.getElementById("star_1").addEventListener('click', () => {
+        document.getElementById("star_1").style.color = "#80D5F9";
+        document.getElementById("star_2").style.color = "black";
+        document.getElementById("star_3").style.color = "black";
+        document.getElementById("star_4").style.color = "black";
+        document.getElementById("star_5").style.color = "black";
+    
+        document.getElementById('rating_stars_hidden').value = "1";
+    });
+    
+    document.getElementById("star_2").addEventListener('click', () => {
+        document.getElementById("star_1").style.color = "#80D5F9";
+        document.getElementById("star_2").style.color = "#80D5F9";
+        document.getElementById("star_3").style.color = "black";
+        document.getElementById("star_4").style.color = "black";
+        document.getElementById("star_5").style.color = "black";
+    
+        document.getElementById('rating_stars_hidden').value = "2";
+    });
+    
+    document.getElementById("star_3").addEventListener('click', () => {
+        document.getElementById("star_1").style.color = "#80D5F9";
+        document.getElementById("star_2").style.color = "#80D5F9";
+        document.getElementById("star_3").style.color = "#80D5F9";
+        document.getElementById("star_4").style.color = "black";
+        document.getElementById("star_5").style.color = "black";
+    
+        document.getElementById('rating_stars_hidden').value = "3";
+    });
+    
+    document.getElementById("star_4").addEventListener('click', () => {
+        document.getElementById("star_1").style.color = "#80D5F9";
+        document.getElementById("star_2").style.color = "#80D5F9";
+        document.getElementById("star_3").style.color = "#80D5F9";
+        document.getElementById("star_4").style.color = "#80D5F9";
+        document.getElementById("star_5").style.color = "black";
+    
+        document.getElementById('rating_stars_hidden').value = "4";
+    });
+    
+    document.getElementById("star_5").addEventListener('click', () => {
+        document.getElementById("star_1").style.color = "#80D5F9";
+        document.getElementById("star_2").style.color = "#80D5F9";
+        document.getElementById("star_3").style.color = "#80D5F9";
+        document.getElementById("star_4").style.color = "#80D5F9";
+        document.getElementById("star_5").style.color = "#80D5F9";
+    
+        document.getElementById('rating_stars_hidden').value = "5";
+    });
+} catch (error) {
+    // pass
+}
 
-document.getElementById("star_1").addEventListener('click', () => {
-    document.getElementById("star_1").style.color = "#80D5F9";
-    document.getElementById("star_2").style.color = "black";
-    document.getElementById("star_3").style.color = "black";
-    document.getElementById("star_4").style.color = "black";
-    document.getElementById("star_5").style.color = "black";
+try {
+    document.getElementById('add_btn').addEventListener('click', () => {
+        document.getElementById('add_book_section').style.display = "block";
+    });
 
-    document.getElementById('rating_stars_hidden').value = "1";
-});
-
-document.getElementById("star_2").addEventListener('click', () => {
-    document.getElementById("star_1").style.color = "#80D5F9";
-    document.getElementById("star_2").style.color = "#80D5F9";
-    document.getElementById("star_3").style.color = "black";
-    document.getElementById("star_4").style.color = "black";
-    document.getElementById("star_5").style.color = "black";
-
-    document.getElementById('rating_stars_hidden').value = "2";
-});
-
-document.getElementById("star_3").addEventListener('click', () => {
-    document.getElementById("star_1").style.color = "#80D5F9";
-    document.getElementById("star_2").style.color = "#80D5F9";
-    document.getElementById("star_3").style.color = "#80D5F9";
-    document.getElementById("star_4").style.color = "black";
-    document.getElementById("star_5").style.color = "black";
-
-    document.getElementById('rating_stars_hidden').value = "3";
-});
-
-document.getElementById("star_4").addEventListener('click', () => {
-    document.getElementById("star_1").style.color = "#80D5F9";
-    document.getElementById("star_2").style.color = "#80D5F9";
-    document.getElementById("star_3").style.color = "#80D5F9";
-    document.getElementById("star_4").style.color = "#80D5F9";
-    document.getElementById("star_5").style.color = "black";
-
-    document.getElementById('rating_stars_hidden').value = "4";
-});
-
-document.getElementById("star_5").addEventListener('click', () => {
-    document.getElementById("star_1").style.color = "#80D5F9";
-    document.getElementById("star_2").style.color = "#80D5F9";
-    document.getElementById("star_3").style.color = "#80D5F9";
-    document.getElementById("star_4").style.color = "#80D5F9";
-    document.getElementById("star_5").style.color = "#80D5F9";
-
-    document.getElementById('rating_stars_hidden').value = "5";
-});
+    document.getElementById('close_book_add').addEventListener('click', () => {
+        document.getElementById('add_book_section').style.display = "none";
+    });
+} catch (error) {
+    // pass
+}
